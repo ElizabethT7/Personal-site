29542eefebf7e9f32dec8ead3f252e9cbc804a6f
ElizabethT7 <tymofieievai@gmail.com>
2023-07-14 14:45:19 +0300

    feat: add sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
   projectId: 'vhpn050s',
@@ -8,6 +9,9 @@ const config = defineConfig({
   apiVersion: '2023-07-14',
   basePath: '/admin',
   plugins: [deskTool()],
+  schema: {
+    types: schemas
+  }
 });
 
 export default config;
