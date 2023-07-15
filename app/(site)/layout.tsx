f45a3ecd804b9b868d115fb1e92e8e3275bafbd1
ElizabethT7 <tymofieievai@gmail.com>
2023-07-15 18:33:20 +0300

    feat: add mediaData

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,6 +9,33 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: `Elisabeth's website`,
   description: 'Frontend developers blog website',
+  keywords: [
+    'Frontend developers',
+    'blog website',
+    `Elisabeth's website`,
+    'Next.js',
+    'React',
+    'JavaScript',
+    'сайт',
+    'заказать сайт'
+  ],
+  applicationName: `Elisabeth's website app`,
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  category: 'technology',
 }
 
 export default function RootLayout({
